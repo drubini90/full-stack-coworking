5e2d3f8c392915573e46beab34e5965800c091a8
rubini124 <rubinid@uw.edu>
2019-07-23 21:40:24 -0700

    Updates

--- api/routes/units.company.employees.js
+++ api/routes/units.company.employees.js
@@ -2,4 +2,13 @@ const router = require("express").Router({ mergeParams: true });
 const Units = require("../models/unit");
 const Company = require("../models/company");
 
+//get employees in a company with given unit id
+router.get("/", async (req, res, next) => {
+  const status = 200;
+  const { company } = await Units.findById(req.params.id).select("company");
+  console.log(company);
+  const employees = company.employees;
+  res.json({ status, employees });
+});
+
 module.exports = router;
