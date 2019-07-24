b97f92b400443a5a6a09405b01168829ce6162b5
rubini124 <rubinid@uw.edu>
2019-07-23 23:38:17 -0700

    Added employee get routes

--- api/routes/employees.js
+++ api/routes/employees.js
@@ -1,32 +1,24 @@
 const router = require("express").Router({ mergeParams: true });
+const Units = require("../models/unit");
+const Company = require("../models/company");
 const Employees = require("../models/employee");
 
 router.get("/", async (req, res, next) => {
   const status = 200;
   let response;
+  let result = await Units.find({
+    company: { $exists: true }
+  }).find({
+    employee: { $exists: true }
+  });
   if (req.query.name) {
-    let result = await Employees.find().select("-__v");
     response = result.filter(emp => emp.full_name.includes(this.name));
   } else if (req.query.birthday) {
-    response = await Employees.find({
-      birthday: req.query.birthday
-    }).select("-__v");
+    response = result.filter(emp => emp.birthday.eq(this.name));
   } else {
     response = await Employees.find().select("-__v");
   }
   res.json(response);
 });
-
-//   app.get("/vegetables", (req, res, next) => {
-//     if (req.query) {
-//       const { vegetables } = data;
-//       const { name } = req.query;
-//       const result = vegetables.filter(veggie => veggie.name.includes(name));
-//       res.json(result);
-//     } else {
-//       const { vegetables } = data;
-//       res.json(vegetables);
-//     }
-//   });
 
 module.exports = router;
